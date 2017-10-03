71a23e8c42382596399e2cc60a73c22ff138f941
Seth Lyons <seth.lyons@gmail.com>
2017-10-03 14:24:45 -0400

    support new git ssh cloning while leaving http and old git protocol intact

--- manifests/clone.pp
+++ manifests/clone.pp
@@ -6,9 +6,17 @@ define dotfiles::clone(
     $homedir,
     $creates,
   ) {
+
+  $ssh = inline_template("<%= @giturl[0..3] %>")
+  if $ssh == 'git@' {
+    $url = "$giturl:"
+  } else {
+    $url = "$giturl/"
+  }
+
   exec { "clone ${gituser} dotfiles for ${title}":
     cwd     => "${homedir}",
-    command => "git clone ${giturl}/${gituser}/${project}.git --branch $branch",
+    command => "git clone ${url}${gituser}/${project}.git --branch $branch",
     creates => "${creates}",
     user    => "${title}",
     require => Package['git'],
